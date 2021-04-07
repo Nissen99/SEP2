7088993796960f6f4020ef91c4dceeb1789aa38a
Mikkel <Mikkelnj411@gmail.com>
2021-04-07 16:43:52 +0200

    Det ser gur ud, lavet nog views

---
+++ src/Client/view/movieList/MovieListController.java
@@ -0,0 +1,14 @@
+package Client.view.movieList;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+
+public class MovieListController
+{
+  @FXML public ChoiceBox movieSelector;
+  @FXML public Button confirmButton;
+
+
+
+}
