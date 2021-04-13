d33feecf17d67dc177a9fc6ccdc43ca73593936a
Mikkel <Mikkelnj411@gmail.com>
2021-04-13 13:24:33 +0200

    Lister be gone

--- src/Client/view/viewModel/ViewModelBooking.java
+++ src/Client/view/viewModel/ViewModelBooking.java
@@ -2,19 +2,17 @@ package Client.view.viewModel;
 
 import Client.core.ModelFactory;
 import Client.model.Model;
-import shared.transferobjects.Booking;
-import shared.transferobjects.User;
 
 import java.sql.SQLException;
 
 public class ViewModelBooking
 {
 
-  Model model = ModelFactory.getInstance().getModel();
+  Model modelManager = ModelFactory.getInstance().getModel();
 
   public void makeBooking(String username) throws SQLException
   {
-    model.addBooking(model.getSelectedShowing(),username);
+    modelManager.addBooking(modelManager.getSelectedShowing(),username);
   }
 
 
