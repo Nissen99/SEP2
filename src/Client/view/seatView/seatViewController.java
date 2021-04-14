8de8b37813a2b56dbc7b79f642ba4ba16062255b
Mathias <mathiashome@hotmail.com>
2021-04-14 15:07:25 +0200

    Yeet

---
+++ src/Client/view/seatView/seatViewController.java
@@ -0,0 +1,22 @@
+package Client.view.seatView;
+
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+
+import java.sql.SQLException;
+
+public class seatViewController {
+
+    private Pane seatTest;
+
+
+    public void init() throws SQLException
+    {
+
+    }
+
+
+
+
+
+}
