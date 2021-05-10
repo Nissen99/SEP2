b82008e3bb3e2689a292ffc80fe3b9723ffecaa5
Mathias <mathiashome@hotmail.com>
2021-05-10 14:57:15 +0200

    nej

--- src/shared/transferobjects/Booking.java
+++ src/shared/transferobjects/Booking.java
@@ -10,14 +10,13 @@ public class Booking implements Serializable
   private int bookingId;
   private Showing showing;
   private User user;
-  //private ObjectProperty<User> userPropterty = new SimpleObjectProperty();
 
 
   public Booking( int bookingId, Showing showing, User user)
   {
     this.showing = showing;
     this.user = user;
-    //userPropterty.set(user);
+
     this.bookingId = bookingId;
   }
 
